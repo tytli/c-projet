0dd1bb81606e7f25755e56d3f6bd6c0107e9a4fe
Nikolay <me0265826@gmail.com>
2024-07-28 09:29:27 +0300

    Add files via upload
    temperature

---
+++ temperature.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+	int temp=12;
+	std::cout<<"Please write temperature\n";
+	if (temp==0||temp==30)
+	{
+      std::cout<<"temperature good";
+	}
+	else
+	{
+      std::cout<<"Temperature is not good";
+	}
+}
