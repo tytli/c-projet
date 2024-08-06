9b29f3c73be86e8b5037f60e5698792261ff9fdd
Nikolay <me0265826@gmail.com>
2024-08-06 22:32:49 +0300

    Update while.cpp

--- while.cpp
+++ while.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
-int main()
 {
-  std::cout<<"Hi world";
+   int v=1;
+  ::cout<<v<<endl;
+   bool sunny=true;
+   bool sunny1=false;
+   int i=5;
+   if (sunny)
+   {
+    cout<<"True good"<<"\n";
+   }
+   if (!sunny1)
+   {
+    cout<<"Is not good";
+   }
+   v?cout<<"Hi":cout<<"Good bye";
+   for ( int i=5;v=1;v>0)
+   {
+    cout<<"Hi world";
+   
+   }
+   while (i>3)
+   {
+    cout<<"White";
+   
+   }
 
 }
